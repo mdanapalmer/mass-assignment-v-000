20c8f646b351b43e32c82764d1dc5857779437ff
Dana Palmer <roiai4@hotmail.com>
2018-10-30 01:26:11 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,7 +4,8 @@ attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
 def initialize(attributes)
-  attributes.each do |key, value| self.send(("#{key}="), value)
-
+  attributes.each do |key, value|
+    self.send(("#{key}="), value)
 end
+
 end
